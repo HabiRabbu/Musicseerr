ebbffee66397bb704c1a4efe981eee90ad4eab1c
Arno <arnolicious.git@gmail.com>
2026-04-12 17:52:33 +0200

    remove log

--- frontend/src/routes/artist/[id]/+page.ts
+++ frontend/src/routes/artist/[id]/+page.ts
@@ -3,7 +3,6 @@ import { queryClient } from '$lib/queries/QueryClient';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params }) => {
-	console.log('Prefetching artist data for artist ID:', params.id);
 	queryClient.prefetchQuery({
 		queryKey: ArtistQueryKeyFactory.basic(params.id)
 	});
